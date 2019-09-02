413809915f0ff3a7a9b180f144c991cf1c339bb0
tpoe <tpoe@camelot-itlab.com>
2019-09-02 20:03:28 +0200

    Improved output unit tests

--- test/output.spec.js
+++ test/output.spec.js
@@ -36,7 +36,7 @@ describe('Test output functionality', () => {
         boards: ['My Board'],
         _isTask: true,
         dueDate: null,
-        isComplete: false,
+        isComplete: true,
         inProgress: false,
         priority: 1
       },
@@ -45,7 +45,7 @@ describe('Test output functionality', () => {
         _date: 'Mon Sep 02 2019',
         _timestamp: 1567434272855,
         description: 'Second Test Task',
-        isStarred: false,
+        isStarred: true,
         boards: ['My Board'],
         _isTask: true,
         dueDate: null,
@@ -56,17 +56,87 @@ describe('Test output functionality', () => {
     });
   });
 
-  it('should display By Board', () => {
+  it('should display by board', () => {
     process.stdout.write = jest.fn();
 
     taskline.displayByBoard().then(() => {
       expect(process.stdout.write.mock.calls.length).toBe(4);
       expect(process.stdout.write.mock.calls[0][0]).toBe(
-        '\n  [4mMy Board[24m [90m[0/2][39m\n'
+        '\n  [4mMy Board[24m [90m[1/2][39m\n'
       );
       expect(process.stdout.write.mock.calls[1][0]).toBe(
-        '    [90m1.[39m [34m* [39m Test Note\n'
+        '    [90m1.[39m [34m● [39m Test Note\n'
       );
+      expect(process.stdout.write.mock.calls[2][0]).toBe(
+        '    [90m2.[39m [32m✔ [39m [90mTest Task[39m\n'
+      );
+      expect(process.stdout.write.mock.calls[3][0]).toBe(
+        '    [90m3.[39m [35m☐ [39m Second Test Task [33m★[39m\n'
+      );
+    });
+  });
+
+  it('should display by date', () => {
+    process.stdout.write = jest.fn();
+
+    taskline.displayByDate().then(() => {
+      expect(process.stdout.write.mock.calls[0][0]).toBe(
+        '\n  [4mMon Sep 02 2019[24m [90m[Today][39m [90m[1/2][39m\n'
+      );
+      expect(process.stdout.write.mock.calls[1][0]).toBe(
+        '    [90m1.[39m [34m● [39m Test Note  \n'
+      );
+      expect(process.stdout.write.mock.calls[2][0]).toBe(
+        '    [90m2.[39m [32m✔ [39m [90mTest Task[39m  \n'
+      );
+      expect(process.stdout.write.mock.calls[3][0]).toBe(
+        '    [90m3.[39m [35m☐ [39m Second Test Task  [33m★[39m\n'
+      );
+    });
+  });
+
+  it('should display stats', () => {
+    taskline.displayByBoard().then(grouped => {
+      process.stdout.write = jest.fn();
+
+      taskline.displayStats(grouped);
+      expect(process.stdout.write.mock.calls[0][0]).toBe(
+        '\n  [90m[33m50%[90m of all tasks complete.[39m\n'
+      );
+      expect(process.stdout.write.mock.calls[1][0]).toBe(
+        '  [32m1[39m [90mdone[39m[90m · [39m[34m0[39m [90min-progress[39m[90m · [39m[35m1[39m [90mpending[39m[90m · [39m[34m1[39m [90mnote[39m \n\n');
+    });
+  });
+
+  it('should display only tasks', () => {
+    process.stdout.write = jest.fn();
+
+    taskline.listByAttributes('tasks').then(grouped => {
+      expect(grouped['My Board'].length).toBe(2);
+    });
+  });
+
+  it('should display only notes', () => {
+    process.stdout.write = jest.fn();
+
+    taskline.listByAttributes('note').then(grouped => {
+      expect(grouped['My Board'].length).toBe(1);
+    });
+  });
+
+  it('should display only starred items', () => {
+    process.stdout.write = jest.fn();
+
+    taskline.listByAttributes('star').then(grouped => {
+      expect(grouped).toMatchObject({});
+    });
+  });
+
+  it('should display only done tasks', () => {
+    process.stdout.write = jest.fn();
+
+    taskline.listByAttributes('done').then(grouped => {
+      expect(grouped['My Board'].length).toBe(1);
     });
   });
 
