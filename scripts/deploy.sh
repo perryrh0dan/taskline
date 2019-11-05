40cf71ea048abc2ab722463a6489d8bc0c7375be
Thomas Pöhlmann <thomaspoehlmann96@googlemail.com>
2019-11-05 09:47:35 +0100

    feat(semantic-release): add first test of semantic release

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e # exit with nonzero exit code if anything fails
+
+# run the build
+npm run build
+
+# deploy with semantic-release
+npm run semantic-release
+
+# just to be sure we exit cleanly
+exit 0;
