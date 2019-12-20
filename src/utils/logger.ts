078bd3602dad5f464be8ac9af7fe90f033841a82
Thomas Pöhlmann <thomaspoehlmann96@googlemail.com>
2019-12-20 16:17:55 +0100

    feat(logger): add logger for debugging

---
+++ src/utils/logger.ts
@@ -0,0 +1,24 @@
+import { createLogger, transports, format } from 'winston';
+
+const isDebug = process.env.DEBUG;
+
+const scheme = format.printf(({
+  level,
+  message,
+  timestamp
+}) => {
+  return `${timestamp} - ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: (isDebug) ? 'debug' : 'error',
+  format: format.combine(
+    format.timestamp(),
+    scheme
+  ),
+  transports: [
+    new transports.Console()
+  ]
+});
+
+export default logger;
